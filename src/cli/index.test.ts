abcc4b44f3427cbc06853a8ff089443311f6ad9a
tylim <limteikyee@gmail.com>
2022-04-25 02:13:27 +0800

    fix test

--- src/cli/index.test.ts
+++ src/cli/index.test.ts
@@ -2,13 +2,13 @@ import 'jest'
 import { main, cli } from './index'
 
 describe('test cli', () => {
+	it('test cli', () => {
+		expect(cli).not.toThrow()
+	})
 	it('test main ok', () => {
 		expect(main({ project: 'firelaw.json' })).toBe(undefined)
 	})
 	it('test main error', () => {
 		expect(typeof main({ project: 'firelaw1.json' })).toBe('string')
-	})
-	it('test cli', () => {
-		expect(cli).not.toThrow()
 	})
 })
